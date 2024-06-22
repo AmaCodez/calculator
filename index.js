6ff73e39dcb821885249392e22e5de65f5168c37
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2024-06-22 15:06:58 -0400

    Add operate and display function

--- index.js
+++ index.js
@@ -13,3 +13,35 @@
 // function divide (a,b){
 // return a / b ;
 //}
+
+// function appendToDisplay(input){
+// display.value += input // shows the calc on the screen
+// input being the operator and numbers
+//}
+
+// function clearDisplay (){
+// display.value += ' ';
+//}
+
+// function calculate (){
+// display.value += operate();
+//}
+
+// element.addEventListener('click', ) => input
+// element.addEventListener('click', ) => clear
+// element.addEventListener('click', ) => decimal point (includes)
+//stopPropagation();
+
+// let firstNumber;
+// let operators;
+// let secondNumber;
+
+// function operate (operator, num1, num2){
+//  firstNumber = num1;
+// operators = operator;
+// secondNumber = num2
+// if (operators == '+') return function add(firstNumber, secondNumber);
+// if (operators == '-') return function subtract(firstNumber, secondNumber);
+// if (operators == '*') return function multiply(firstNumber, secondNumber);
+// if (operators == '/') return function divide(firstNumber, secondNumber);
+//}
