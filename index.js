0ac60e6fa9534f4416c44798e3243abb8c531b3c
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2024-06-24 15:40:37 -0400

    Update the clear function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const display = document.querySelector('#display');
-const btn = document.querySelectorAll('button');
-const clear = document.querySelector('.clear');
-const eval = document.querySelector('.eval');
+const btn = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear');
+const equalBtn = document.querySelector('.eval');
 
 function add (a,b){
 return a + b ;
@@ -16,39 +16,50 @@ return a * b ;
 }
 
 function divide (a,b){
-return a / b ;
+    if (b !== 0) {
+        return a / b ;
+    } else {
+        return 'Error';
+    }
+
 }
 
-// let firstNumber;
-// let operators;
-// let secondNumber;
+let firstNumber;
+let operators;
+let secondNumber;
 
-// function operate (operator, num1, num2){
-//  firstNumber = num1;
-// operators = operator;
-// secondNumber = num2
-// if (operators == '+') return function add(firstNumber, secondNumber);
-// if (operators == '-') return function subtract(firstNumber, secondNumber);
-// if (operators == '*') return function multiply(firstNumber, secondNumber);
-// if (operators == '/') return function divide(firstNumber, secondNumber);
-//}
+function operate (operator, num1, num2){
+ firstNumber = num1;
+operators = operator;
+secondNumber = num2;
+if (operators == '+') {
+    return add(firstNumber, secondNumber)
+} else if (operators == '-') {
+    return subtract(firstNumber, secondNumber)
+} else if (operators == '*') {
+    return multiply(firstNumber, secondNumber)
+} else if (operators == '/') {
+    return divide(firstNumber, secondNumber)
+}
+};
 
-// let value = display.value;
-// function appendToDisplay(input){
-// value += input // shows the calc on the screen
-// input being the operator and numbers
-//}
+function appendToDisplay(event){
+const input = event.target.textContent;  // shows the calc on the screen input being the operator and numbers
+display.value += input;
+}
 
-// function clearDisplay (){
-// value += ' ';
-//}
+function calculate (){
+display.value = operate();
+}
 
-// function calculate (){
-// value += operate();
-//}
+btn.forEach((key) => {
+    key.addEventListener('click', appendToDisplay);
+});
 
-// element.addEventListener('click', ) => input
-// element.addEventListener('click', ) => clear
+clearBtn.addEventListener('click', () => {
+    display.value = '';
+}); 
+// element.addEventListener('click', ) => eval
 // element.addEventListener('click', ) => decimal point (includes)
 //stopPropagation();
 
